4c11d2fbb7219ddfde25e92a652015c207e84c18
Kiptoo Magutt <kiptoo@twende.app>
2021-06-14 13:23:55 +0300

    codelabs2 - add interactivity to the faved icons

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,16 @@ class _RandomWordsState extends State<RandomWords> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
+      onTap: () {
+        // listen to taps on the faved icon
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
 }
