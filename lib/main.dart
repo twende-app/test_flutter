670a135fa97fd6b11439745edde86b08ed8a73c2
Kiptoo Magutt <kiptoo@twende.app>
2021-06-14 12:14:17 +0300

    quality | codelab - remove redundant title param on material app
    - seems the one under the state builder takes precedence

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
       home: RandomWords(),
     );
   }
